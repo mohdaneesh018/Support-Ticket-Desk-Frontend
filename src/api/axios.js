ef0b39c716ebb2bb0645ba5c2946fa796584acf1
mohdaneesh018 <mdaneesh537@gmail.com>
2026-02-21 00:41:14 +0530

    Updated Backend URL

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://support-ticket-desk-backend-0g5r.onrender.com/api",
 });
 
 API.interceptors.request.use((req) => {
